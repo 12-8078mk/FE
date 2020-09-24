2c07894373264f40b044ea5b78fca20ddd17ebd9
12-8078mk <68376481+12-8078mk@users.noreply.github.com>
2020-09-24 18:34:41 +0900

    ?

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,7 +5,7 @@ class CarsController < ApplicationController
   
   def new
     @car = Car.new
-    # @car.users << current_user
+    # @car << current_user.params[:id]
   end
 
   def create
@@ -32,6 +32,6 @@ class CarsController < ApplicationController
 
   private
   def car_params
-    params.require(:car).permit(:name, user_ids: [])
+    params.require(:car).permit(:car_name, user_ids: [])
   end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,3 @@
 class Car < ApplicationRecord
-  belongs_to :user
+  belongs_to :users
 end
