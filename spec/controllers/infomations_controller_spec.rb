06ebc890daa00a1d5bc6f3c79cbb6a92f186c47b
12-8078mk <6c787b.itm.744a@gmail.com>
2020-10-24 22:09:43 +0900

    spec.com

--- spec/controllers/infomations_controller_spec.rb
+++ spec/controllers/infomations_controller_spec.rb
@@ -35,4 +35,55 @@ describe InfomationsController do
       end
     end
   end
+  describe '#create' do
+    let(:params) { { car_id: car.id, user_id: user.id, infomation: attributes_for(:infomation) } }
+
+    context 'log in' do
+      before do
+        login user
+      end
+
+      context 'can save' do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it 'count up infomation' do
+          expect{ subject }.to change(Infomation, :count).by(1)
+        end
+
+        it 'redirects to car_infomations_path' do
+          subject
+          expect(response).to redirect_to(car_infomations_path(car))
+        end
+      end
+
+      context 'can not save' do
+        let(:invalid_params) { { car_id: car.id, user_id: user.id, infomation: attributes_for(:infomation, refuel: nil, refuelday: nil, trip: nil, cost: nil, fueltype_id: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it 'does not count up' do
+          expect{ subject }.not_to change(Infomation, :count)
+        end
+
+        it 'renders index' do
+          subject
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    context 'not log in' do
+
+      it 'redirects to new_user_session_path' do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
