19440f952d8ec3661a99dca94921dd5042d7342d
12-8078mk <6c787b.itm.744a@gmail.com>
2020-10-30 21:31:56 +0900

    graph OK

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,16 +2,17 @@ class GraphsController < ApplicationController
   before_action :set_car
 
   def index
-    # @data = [['2019-06-01', 100], ['2019-06-02', 200], ['2019-06-04', 150]]
-    @cost_data = []
+    @fe_data = []
     @avcost_data = []
     graphs = @car.infomations.includes(:user)
     graphs.order("refuelday").each do |inf|
       cost = inf.cost
       refuel = inf.refuel
       refuelday = inf.refuelday
+      trip = inf.trip
       average_cost = cost / refuel
-      @cost_data << [ refuelday , cost]
+      average_fe = trip / refuel
+      @fe_data << [ refuelday , average_fe]
       @avcost_data << [refuelday , average_cost]
     end
   end
